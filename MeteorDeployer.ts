827c39ecce5b982da1f9e4d502cd8328d35bc080
Joel Saltzman <joel@grayv.com>
2019-04-15 00:33:47 -0700

    Added some comments

--- MeteorDeployer.ts
+++ MeteorDeployer.ts
@@ -5,27 +5,47 @@ import MeteorSettings from './MeteorSettings';
 
 class MeteorDeployer {
 
-    configPath: string
-    buildPath: string; //Path to where you want the bundle built at
-    config: MeteorSettings;
+    /**
+     * @property {string} settingsPath Path to the settings file passed with `meteor --setting production.json`
+     * @property {string} buildPath Path to where you want the bundle built at
+     * @property {MeteorSettings} settings Settings json decoded into a MeteorSettings object
+     */
+    settingsPath: string
+    buildPath: string; 
+    settings: MeteorSettings;
 
-    constructor(configPath: string, buildPath: string) {
-        this.configPath = configPath;
-        this.config = MeteorSettings.parseConfigFile(configPath);
+
+    /**
+     * Decodes the settings file at settingsPath into the settings property. `buildPath` is stored for use later
+     * @param {string} Path to the settings file passed with `meteor --setting production.json`
+     * @param {string} Path to where you want the bundle built at
+     */
+    constructor(settingsPath: string, buildPath: string) {
+        this.settingsPath = settingsPath;
+        this.settings = MeteorSettings.parseConfigFile(settingsPath);
         this.buildPath = buildPath;
     }
 
+    /**
+     * Builds the bundle using the values in the settings file.
+     */
     createBuild() {
         console.log('=> Creating Bundle');
-        const command = `meteor build --allow-superuser --directory ${this.buildPath}/${this.config.name} --server http://${this.config.name}:${this.config.PORT}`;
+        const command = `meteor build --allow-superuser --directory ${this.buildPath}/${this.settings.name} --server http://${this.settings.name}:${this.settings.PORT}`;
         execSync(command, {stdio: 'inherit'});
     }
+    /**
+     * Copies the settings file into the bundle root as `settings.json`
+     */
     copySettings() {
         console.log('=> Copying settings file');
-        const destination = path.join(this.buildPath, this.config.name, 'bundle', 'settings.json');
-        fs.copyFileSync(this.configPath, destination);
-        console.log(`\t${this.config} copied to ${destination}`);
+        const destination = path.join(this.buildPath, this.settings.name, 'bundle', 'settings.json');
+        fs.copyFileSync(this.settingsPath, destination);
+        console.log(`\t${this.settings} copied to ${destination}`);
     }
+    /**
+     * Creates the `package.json` file at the root of the bundle for launching the app.
+     */
     createPackageFile() {
         console.log('=> Creating package.json');
         
@@ -37,10 +57,15 @@ class MeteorDeployer {
             }
         };
         const file = JSON.stringify(packageFile, null, ' ');
-        const destination = path.join(this.buildPath, this.config.name, 'bundle', 'package.json');
+        const destination = path.join(this.buildPath, this.settings.name, 'bundle', 'package.json');
         fs.writeFileSync(destination, file);
         console.log(`\tpackage.json created at ${destination}`);
     }
+
+    /**
+     * Creates a `dockerfile` in the root of the bundle for building an image. The image will contain the bundle
+     * within the docker image.
+     */
     createDockerfile() {
         console.log('=> Creating Dockerfile');
         let file = `
@@ -55,11 +80,11 @@ class MeteorDeployer {
         RUN cd /usr/app/programs/server && npm install --production
         WORKDIR /usr/app/
         ENV PORT=3000
-        ENV MONGO_URL=${this.config.MONGO_URL}
-        ENV ROOT_URL=${this.config.ROOT_URL}:${this.config.PORT}/
+        ENV MONGO_URL=${this.settings.MONGO_URL}
+        ENV ROOT_URL=${this.settings.ROOT_URL}:${this.settings.PORT}/
         CMD ["npm", "start"]
-        EXPOSE 3000`;
-        const destination = path.join(this.buildPath, this.config.name, 'bundle', 'Dockerfile');
+        EXPOSE ${this.settings.PORT}`;
+        const destination = path.join(this.buildPath, this.settings.name, 'bundle', 'Dockerfile');
         fs.writeFileSync(destination, file);
         console.log(`\tDockerfile created at ${destination}`);
     }
